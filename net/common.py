478c609980da644685e0c09ad36fdc6b4047dc34
胡方全 <hufangquan@MBP.local>
2018-12-03 00:12:03 +0800

    debug

--- net/common.py
+++ net/common.py
@@ -478,8 +478,8 @@ def ssd_anchor_match_layer(gtlabels,
           - only update if beat the score of other bboxes.
         """
         # Jaccard score.
-        label = gtlabels[:,i].reshape([-1,])
-        bbox = gtboxes[:,i,:].reshape([0,-1])
+        label = tf.reshape(gtlabels[:,i],[-1,])
+        bbox = tf.reshape(gtboxes[:,i,:],[0,-1])
         try:
             jaccard = jaccard_with_anchors(bbox) # IOU
         except Exception:
