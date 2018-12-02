241c3c02d390b86eead8c7318373ecab4643a1a1
胡方全 <hufangquan@MBP.local>
2018-12-02 17:02:40 +0800

    debug

--- net/common.py
+++ net/common.py
@@ -482,7 +482,7 @@ def ssd_anchor_match_layer(gtlabels,
         try:
             jaccard = jaccard_with_anchors(bbox) # IOU
         except Exception:
-            assert False, 'anchor_for:'+anchor_for+' ymin: '+str(ymin.shape)+' bbox:'+str(bbox)
+            assert False, 'anchor_for:'+anchor_for+' ymin: '+str(ymin.shape)+' bbox:'+str(bbox[0])
         # Mask: check threshold + scores + no annotations + num_classes.
         mask = tf.greater(jaccard, feat_scores)
         # mask = tf.logical_and(mask, tf.greater(jaccard, matching_threshold))
