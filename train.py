43165394fca18aa6ee3ccb0770444f7dfcf701f0
胡方全 <hufangquan@MBP.local>
2018-12-10 18:04:32 +0800

    debug

--- train.py
+++ train.py
@@ -233,7 +233,7 @@ def get_model_fn(num_gpus, variable_strategy, num_workers):
                 # op = tf.summary.scalar(summary_name, mAP, collections=[])
                 # op = tf.Print(op, [mAP], summary_name)
                 # tf.add_to_collection(tf.GraphKeys.SUMMARIES, op)
-
+        print('predictions:',predictions)
         for k, v in predictions:
             for tensor in v:
                 tshape = utils_func.get_shape(tensor)
